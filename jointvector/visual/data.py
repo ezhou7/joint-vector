ec131b5d51cc95ade100a2a2d25336f3c75f8007
Ethan Zhou <ethan.zhou@emory.edu>
2018-09-01 22:21:00 -0700

    data preprocessing midway

--- jointvector/visual/data.py
+++ jointvector/visual/data.py
@@ -1,3 +1,20 @@
-def sync_with_text():
-    # TODO: search imagenet
+import numpy as np
+
+
+def search_imagenet(word):
+    # TODO: implement this method
     pass
+
+
+def resize_image(img):
+    # TODO: resize image to 256x256
+    pass
+
+
+def sync_with_text(sentences):
+    empty_image = np.zeros(shape=(256, 256))
+
+    for sentence in sentences:
+        for token in sentence:
+            token_image = search_imagenet(token.word_form) if token.pos.startswith("NN") else empty_image
+            setattr(token, "image", token_image)
